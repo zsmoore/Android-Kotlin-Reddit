b1e8661a4847494a71633ee6f461e5b34cf6b0db
zsmoore <zsmoore@zachary-moore.com>
2017-11-24 00:24:38 -0500

    removed unnecessary print line

--- app/src/main/java/com/zachary_moore/androidkotlinreddit/view/SubredditCardViewModel.kt
+++ app/src/main/java/com/zachary_moore/androidkotlinreddit/view/SubredditCardViewModel.kt
@@ -9,7 +9,6 @@ class SubredditCardViewModel (name: String){
     val subRedditName = name
     private val link = "https://www.reddit.com/r/$subRedditName"
     fun goToLink(activity: MainActivity) {
-        Logger.getLogger(MainActivity::class.java.name).warning("here")
         val intent = Intent(Intent.ACTION_VIEW)
         intent.data = Uri.parse(link)
         activity.startActivity(intent)
